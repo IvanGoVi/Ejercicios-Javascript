d6dd85b3c58d53a4bad7738f667b02330cb49140
IvanGV <ivan.gvillalobos@gmail.com>
2023-02-27 19:37:50 -0600

    Multiple longest words function added to ejer3.js

--- ejer3.js
+++ ejer3.js
@@ -1,5 +1,6 @@
 //3.- Crear una función que recibe un string con multiples palabras y regresa únicamente la palabra mas larga en la oración
 
+/*
 const longestWord=(string)=>{
     let splitString=string.split(" ");
     let longestWord="";
@@ -14,3 +15,36 @@ const longestWord=(string)=>{
 
 let example=longestWord("Anita lava la tina grandisima");
 console.log(example);
+*/
+
+
+//Resulto por Isra
+
+let text="este es un texto con muchas palabras de mucha longitud"
+
+const getLongestWords=(textToEvaluate)=>{
+    let textToArray=textToEvaluate.split(" ");
+    let longestWord="";
+    let longestWordsArray=[];
+    for (let i=0; i<textToArray.length; i++){
+        if(textToArray[i].length>longestWord.length){
+            longestWord=textToArray[i];
+        }
+    }
+    console.log(longestWord.length);
+    let largestLength=longestWord.length;
+
+    for (let i=0; i<textToArray.length; i++){
+        textToArray[i].length===largestLength
+        ? longestWordsArray.push(textToArray[i])
+        :null;
+    }
+
+    return longestWordsArray.length>1
+    ? `Las palabras mas largas de este texto son: ${longestWordsArray.join()}`
+    : `La palabra mas larga de este texto es: ${longestWordsArray[0]}`;
+
+};
+
+let result=getLongestWords(text);
+console.log(result);
